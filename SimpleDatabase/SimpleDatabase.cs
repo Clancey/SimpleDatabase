e576596aa1419b82647fc7e15c08041687d214a7
James Clancey <james.clancey@gmail.com>
2017-02-06 15:40:23 -0900

    Added new AsyncTableQuery that doesn't lock!

--- SimpleDatabase/SimpleDatabase.cs
+++ SimpleDatabase/SimpleDatabase.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 //using Java.Lang;
 using System.Threading;
 using System.Diagnostics;
+using System.Linq.Expressions;
 
 namespace SimpleDatabase
 {
@@ -1096,7 +1097,81 @@ namespace SimpleDatabase
 		}
 
 		#endregion
+		public class AsyncTableQuery<T>
+		where T : new()
+		{
+			TableQuery<T> _innerQuery;
 
+			public AsyncTableQuery(TableQuery<T> innerQuery)
+			{
+				_innerQuery = innerQuery;
+			}
+
+			public AsyncTableQuery<T> Where(Expression<Func<T, bool>> predExpr)
+			{
+				return new AsyncTableQuery<T>(_innerQuery.Where(predExpr));
+			}
+
+			public AsyncTableQuery<T> Skip(int n)
+			{
+				return new AsyncTableQuery<T>(_innerQuery.Skip(n));
+			}
+
+			public AsyncTableQuery<T> Take(int n)
+			{
+				return new AsyncTableQuery<T>(_innerQuery.Take(n));
+			}
+
+			public AsyncTableQuery<T> OrderBy<U>(Expression<Func<T, U>> orderExpr)
+			{
+				return new AsyncTableQuery<T>(_innerQuery.OrderBy<U>(orderExpr));
+			}
+
+			public AsyncTableQuery<T> OrderByDescending<U>(Expression<Func<T, U>> orderExpr)
+			{
+				return new AsyncTableQuery<T>(_innerQuery.OrderByDescending<U>(orderExpr));
+			}
+
+			public Task<List<T>> ToListAsync()
+			{
+				return Task.Factory.StartNew(() =>
+				{
+					return _innerQuery.ToList();
+				});
+			}
+
+			public Task<int> CountAsync()
+			{
+				return Task.Factory.StartNew(() =>
+				{
+					return _innerQuery.Count();
+				});
+			}
+
+			public Task<T> ElementAtAsync(int index)
+			{
+				return Task.Factory.StartNew(() =>
+				{
+					return _innerQuery.ElementAt(index);
+				});
+			}
+
+			public Task<T> FirstAsync()
+			{
+				return Task<T>.Factory.StartNew(() =>
+				{
+					return _innerQuery.First();
+				});
+			}
+
+			public Task<T> FirstOrDefaultAsync()
+			{
+				return Task<T>.Factory.StartNew(() =>
+				{
+					return _innerQuery.FirstOrDefault();
+				});
+			}
+		}
 	}
 
 	[AttributeUsage (AttributeTargets.Property)]
